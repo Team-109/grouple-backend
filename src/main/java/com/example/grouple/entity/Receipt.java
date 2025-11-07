ec660938ca50e552dda0094ef8471a8067e74e86
Minkyu Kim <iosif2@iosif.dev>
2025-11-08 00:29:51 +0900

    feature: ERD 기반 엔티티 작성

--- src/main/java/com/example/grouple/entity/Receipt.java
+++ src/main/java/com/example/grouple/entity/Receipt.java
@@ -1,4 +1,47 @@
 package com.example.grouple.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "receipts")
 public class Receipt {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private Integer id;
+
+    @Column(length = 10, nullable = false)
+    private String type;
+
+    @Column(nullable = false)
+    private Integer amount;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column()
+    private String image;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "organization_id", nullable = false)
+    private Organization organization;
 }
