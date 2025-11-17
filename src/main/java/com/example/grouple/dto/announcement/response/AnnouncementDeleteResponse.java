aa849fe7e7c19dff9ce59c28fa979fe9a7c8852e
brad36i <sambean0911@gmail.com>
2025-11-17 17:06:28 +0900

    Announcement Service, Repository, Controller, dto

---
+++ src/main/java/com/example/grouple/dto/announcement/response/AnnouncementDeleteResponse.java
@@ -0,0 +1,10 @@
+package com.example.grouple.dto.announcement.response;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class AnnouncementDeleteResponse {
+}
--- src/main/java/com/example/grouple/repository/AnnouncementRepository.java
+++ src/main/java/com/example/grouple/repository/AnnouncementRepository.java
@@ -1,7 +1,19 @@
 package com.example.grouple.repository;
 
 import com.example.grouple.entity.Announcement;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AnnouncementRepository extends JpaRepository<Announcement, Integer> {
+    Page<Announcement> findByOrganizationIdOrderByCreatedAtDesc(Integer organizationId, Pageable pageable);
+
+    Page<Announcement> findByOrganizationIdAndStar(Integer organizationId, Boolean star, Pageable pageable);
+
+    Optional<Announcement> findByIdAndOrganizationId(Integer id, Integer organizationId);
+
+    Page<Announcement> findByOrganizationIdAndTitleContaining(Integer organizationId, String keyword, Pageable pageable);
+
 }
