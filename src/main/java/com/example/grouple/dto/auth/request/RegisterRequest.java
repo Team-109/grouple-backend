b24c28442ea0645fcc133fb6987859c2c4e78db9
Minkyu Kim <iosif2@iosif.dev>
2025-12-02 21:28:25 +0900

    불필요한 회원가입 요청 DTO 변경사항 제거

--- src/main/java/com/example/grouple/dto/auth/request/RegisterRequest.java
+++ src/main/java/com/example/grouple/dto/auth/request/RegisterRequest.java
@@ -5,13 +5,10 @@ import lombok.*;
 @Data
 public class RegisterRequest {
     private String username;
-    private String password;
-    private String passwordConfirm;
-
-    private String name;
     private String email;
     private String phone;
-
     private String image;
+    private String password;
+    private String passwordConfirm;
 }
 
